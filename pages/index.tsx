9a45c39e142ab937967763572a5607f5f14ba534
Zeidan <ozeidan@mafcarrefour.com>
2019-06-28 01:59:35 +0400

    feat: Adding typescript and the linting with some rules

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+class Home extends Component {
+  public render() {
+    return (
+      <div>
+        Hello 😗 Next JS is now with Typescript - Ready to be linted as well! 🎉
+      </div>
+    );
+  }
+}
+export default Home;

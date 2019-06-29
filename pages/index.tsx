6ea3065190c0c498aa490ec80e45f1de50eb1eec
Zeidan <ozeidan@mafcarrefour.com>
2019-06-29 13:23:40 +0400

    feat: 🎉Adding emotion to style the components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import { Component } from 'react';
+import styled from '@emotion/styled';
+
+const Title = styled('h1')`
+  color: red;
+`;
 
 class Home extends Component {
   public render() {
     return (
-      <div>
+      <Title>
         Hello 😗 Next JS is now with Typescript - Ready to be linted as well! 🎉
-      </div>
+      </Title>
     );
   }
 }
